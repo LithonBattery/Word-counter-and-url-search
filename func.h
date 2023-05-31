2433271be80c81a2399ab30bbfdb6b943308338b
LithonBattery <121124609+LithonBattery@users.noreply.github.com>
2023-05-31 11:08:31 +0300

    Add files via upload

---
+++ func.h
@@ -0,0 +1,8 @@
+#pragma once
+#include "mlib.h"
+
+void read(map <string, int>& m, int& sum, vector <string>& u, map<string, set<int>>& wocc);
+void clean(string& word);
+bool withURL(const string& text, const string& link);
+bool endsWithSymbol(const string& str, char symbol);
+void print(map<string, int>& m, int& sum, vector <string>& u, map<string, set<int>>& wocc);
