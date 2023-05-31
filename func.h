5ea36e2d6c3a6dfacbfafe426a5a6a9b1319f897
LithonBattery <121124609+LithonBattery@users.noreply.github.com>
2023-05-31 11:06:42 +0300

    Delete func.h

--- func.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "mlib.h"
-
-void read(map <string, int>& m, int& sum, vector <string>& u, map<string, set<int>>& wocc);
-void clean(string& word);
-bool withURL(const string& text);
-bool endsWithSymbol(const string& str, char symbol);
-void print(map<string, int>& m, int& sum, vector <string>& u, map<string, set<int>>& wocc);
